ad57f5236a248100d38837aa9dc7e6d296b390d3
vasilevalcan <vasile.p.valcan@gmail.com>
2023-03-17 00:14:35 +0200

    instaled cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportHeight:1080,
+  viewportWidth:1920,
+  e2e: {
+      baseUrl: 'https://way2automation.com/way2auto_jquery/automation-practice-site.html',
+      excludeSpecPattern:['**/1-getting-started/*','**/2-advanced-examples/*']
+  },
+});
